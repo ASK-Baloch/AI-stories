7ce44bab45bc8ffb643fcfeb57ca4e62db287c95
Ahmed Baloch <balochsaeed888@gmail.com>
2024-09-17 21:36:14 +0500

    installed nextui and used it for my project

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@nextui-org/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
-    <h1>Hello world</h1>
+    <h1 className="text-3xl font-bold flex flex-col justify-center items-center">Hello world
+      <Button className="mt-2">Hey</Button>
+    </h1>
    </div>
   );
 }
