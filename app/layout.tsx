c4728ddc4c01dda1443b372603b1ca0eba2ca3d3
Ahmed Baloch <balochsaeed888@gmail.com>
2024-09-18 11:59:31 +0500

    navbar done responsive .. very good component from nextui

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={SacramentoFont.className}
+        className={NunitoFont.className}
       >
         <Provider>{children}</Provider>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@nextui-org/button";
-import Image from "next/image";
+import Header from "./_components/Header";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl  mt-8 font-bold flex flex-col justify-center items-center underline">
-        Hello world
-      </h1>
-      <Button className="mt-8">Hey</Button>
+    <div className="bg-[#cad3ff]">
+     <Header />
     </div>
   );
 }
