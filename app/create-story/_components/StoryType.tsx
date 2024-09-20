4064cfcde5a3763fbdb17a3eda1cf2e63cd52955
Ahmed Baloch <balochsaeed888@gmail.com>
2024-09-20 11:34:16 +0500

    story type also styled now to add selection logic on one of options

--- app/create-story/_components/StoryType.tsx
+++ app/create-story/_components/StoryType.tsx
@@ -1,8 +1,40 @@
+import Image from 'next/image'
 import React from 'react'
 
 function StoryType() {
+    const OptionList = [
+        {
+            label:"Story Book",
+            imageUrl:"/story.png",
+            isFree: true,
+        },
+        {
+            label:"Bed Story ",
+            imageUrl:"/bedstory.png",
+            isFree: true,
+        },
+        {
+            label:"Educational",
+            imageUrl:"/educational.png",
+            isFree: true,
+        },
+    ]
+
   return (
-    <div>StoryType</div>
+    <div>
+        <label className="font-bold text-4xl text-primary">
+        2. Story Type
+      </label>
+      <div className='grid grid-cols-3 gap-5 mt-3'>
+        {OptionList.map((item,index) => (
+            <div key={index} className='relative grayscale hover:grayscale-0 cursor-pointer'>
+                <h2 className='absolute bottom-5 text-center w-full text-2xl text-white'>{item.label}</h2>
+                <Image src={item.imageUrl} alt={item.label} width={300} height={500} 
+                className='object-cover h-[260px] rounded-3xl'/>
+            </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
